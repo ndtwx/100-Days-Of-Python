20e5a71aceb7d1f4eb1686d06a65b7436f02ffcd
ndtwx <nd_twx@hotmail.com>
2023-10-20 20:08:08 +0800

    Weeks Calculator

---
+++ Life in Weeks/Weeks Calculator.py
@@ -0,0 +1,12 @@
+# age = input()
+age = 27
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+
+years = 90 - int(age)
+weeks = years * 52
+
+print("you have " + str(weeks) + " weeks left")
+
+#f string (Auto convert the data type)
+print(f"you have {weeks} weeks left")
