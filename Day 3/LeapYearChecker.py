1f64804e31ae76d8a26d84739f7636c0da0d5739
ndtwx <nd_twx@hotmail.com>
2023-10-21 17:26:57 +0800

    Modified the code

--- Day 3/LeapYearChecker.py
+++ Day 3/LeapYearChecker.py
@@ -1,10 +1,12 @@
 year = int(input("Enter a Year:"))
 
-if (year%4 == 0):
-  print("Leap year")
-  if(year%100 == 0):
-    print("Leap year")
-    if(year%400==0):
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
       print("Leap year")
+    else:
+      print("Not leap year")
+  else:
+    print("Leap year")
 else:
-    print("Not leap year")
+  print("Not leap year")
