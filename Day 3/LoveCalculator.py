ce7a8364c608938a545aa7ca316ef0e1e14f4228
ndtwx <nd_twx@hotmail.com>
2023-10-21 19:48:30 +0800

    Love Calculator

---
+++ Day 3/LoveCalculator.py
@@ -0,0 +1,35 @@
+print("The Love Calculator is calculating your score...")
+name1 = input("Enter the first name") # What is your name?
+name2 = input("Enter the second name") # What is their name?
+
+#Combine both name for easier comparing
+combined_name = name1 + name2
+
+# make all the letters to lower case
+lower_names = combined_name.lower()
+t = lower_names.count("t")
+r = lower_names.count("r")
+u = lower_names.count("u")
+e = lower_names.count("e")
+first_digit = t + r + u + e 
+
+l = lower_names.count("l")
+o = lower_names.count("o")
+v = lower_names.count("v")
+e = lower_names.count("e")
+second_digit = l + o + v + e 
+
+score = str(first_digit) + str(second_digit)
+score = int(score)
+
+if score < 10 or score > 90:
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif score > 40 and score < 50:
+    print(f"Your score is {score}, you are alright together.")
+else: 
+    print(f"Your score is {score}.")
+
+
+
+
+
