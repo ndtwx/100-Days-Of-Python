43fcc7ca776cf9e9e115368d6481bb688d3ea872
ndtwx <nd_twx@hotmail.com>
2023-11-18 19:52:24 +0800

    HigherOrLower Game

---
+++ Day 14/HigherLowerGame/higher_or_lower Game.py
@@ -0,0 +1,10 @@
+"""
+APPROACH TO TAKE:
+1.Breakdown Large problem into smaller problem 
+2.Look at those problems and pick the one that is the eaist to begin with
+3.Turn the problem into comments, split it into many pieces
+    # Import the random module.
+    # Use randint() to get random integer.
+4.Write Code, Run code, Fix code and Repeat
+5. Next Task
+"""
