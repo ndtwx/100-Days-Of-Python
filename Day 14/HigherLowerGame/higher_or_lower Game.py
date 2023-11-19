24fc6198bc95db44c74f1526063df7e29d848a1f
ndtwx <nd_twx@hotmail.com>
2023-11-19 18:00:34 +0800

    HIgher or Lower Game

--- Day 14/HigherLowerGame/higher_or_lower Game.py
+++ Day 14/HigherLowerGame/higher_or_lower Game.py
@@ -1,10 +1,74 @@
-"""
-APPROACH TO TAKE:
-1.Breakdown Large problem into smaller problem 
-2.Look at those problems and pick the one that is the eaist to begin with
-3.Turn the problem into comments, split it into many pieces
-    # Import the random module.
-    # Use randint() to get random integer.
-4.Write Code, Run code, Fix code and Repeat
-5. Next Task
-"""
+import os, random
+from game_data import data
+from art import logo, vs
+
+# Generate a random account from the game data.
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
+
+# Format account data into printable format.
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+#   print(f'{name}: {account["follower_count"]}')
+  return f"{name}, a {description}, from {country}"
+
+# Check if user is correct.
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong.""" 
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+
+
+def game():
+  score = 0
+  game_should_continue = True 
+  account_a = get_random_account()
+  account_b = get_random_account() 
+  print(logo)
+  
+  # Make game repeatable.
+  while game_should_continue:
+      
+    # Make B become the next A.
+    account_a = account_b
+    account_b = get_random_account()
+
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+    
+    # Ask user for a guess.
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    
+    ## Get follower count.
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    # Clear screen between rounds.
+    os.system('cls')
+    print(logo)
+    if is_correct:
+      score += 1
+      print("---------------------------------------")
+      print(f"You're right! Current score: {score}.")
+      print("---------------------------------------")
+    else:
+      game_should_continue = False
+      print("---------------------------------------")
+      print(f"Sorry, that's wrong. Final score: {score}")
+      print("---------------------------------------")
+
+game()
+
