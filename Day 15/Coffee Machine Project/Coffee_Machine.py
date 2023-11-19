96482dcf8a0b2fe6c5445c8691ec4e832c9b0d87
ndtwx <nd_twx@hotmail.com>
2023-11-19 20:36:23 +0800

    Coffee Machine Project

---
+++ Day 15/Coffee Machine Project/Coffee_Machine.py
@@ -0,0 +1,7 @@
+# TODO:1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
+# TODO:2. Turn off the Coffee Machine by entering “off” to the prompt.
+# TODO:3. Print report.
+# TODO:4. Check resources sufficient?
+# TODO:5. Process coins.
+# TODO:6. Check transaction successful?
+# TODO:7. Make Coffee.
