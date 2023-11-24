581670395b679bfb22de549ca9689234998d805c
ndtwx <nd_twx@hotmail.com>
2023-11-24 15:24:58 +0800

    Coffee Machine Project

--- Day 15/Coffee Machine Project/Coffee_Machine.py
+++ Day 15/Coffee Machine Project/Coffee_Machine.py
@@ -11,7 +11,7 @@ def check_resources(order_ingredients):
 def process_coins():
     """Returns the total calculated from coins inserted."""
     print("Please insert coins.")
-    total = int(input("How many dollar?:")) * 1.00
+    total = int(input("How many 1 dollar?:")) * 1.00
     total += int(input("How many 50 cents:"))* 0.50
     total += int(input("How many 20 cents:")) * 0.20
     total += int(input("How many 10 cents:")) * 0.10
@@ -36,27 +36,52 @@ def make_coffee(drink_name, order_ingredients):
     for item in order_ingredients:
         resources[item] -= order_ingredients[item]
     print(f"Here is your {drink_name} ☕️. Enjoy!")
+    print("-----------------------------------")
 
 is_on = True
 
 while is_on:
     # TODO:1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
-    print(f"Espresso: ${MENU["espresso"]["cost"]}")
-    print(f"Latte: ${MENU["latte"]["cost"]}")
-    print(f"Cappuccino: ${MENU["cappuccino"]["cost"]}")
-    choice = input("What would you like to drink?: ")
+    print(f"1.Espresso: ${MENU["espresso"]["cost"]}")
+    print(f"2.Latte: ${MENU["latte"]["cost"]}")
+    print(f"3.Cappuccino: ${MENU["cappuccino"]["cost"]}")
+    choice = input("What would you like to drink? Choose 1 or 2 or 3 : ")
     # TODO:2. Turn off the Coffee Machine by entering “off” to the prompt.
     if choice == "off":
         is_on = False
     # TODO:3. Print report.
     elif choice == "report":
+        print("REPORT:")
         print(f"Water: {resources['water']}ml")
         print(f"Milk: {resources['milk']}ml")
         print(f"Coffee: {resources['coffee']}g")
-        print(f"Money: ${profit}")
+        print(f"Profit: ${profit}")
+        print("-----------------------------------")
+
+    elif choice == "refill":
+        if (resources["water"] == 300 and
+                resources["milk"] == 200 and
+                resources["coffee"] == 100):
+            print("Ingredients are full")
+        else:
+            resources["water"] = 300
+            resources["milk"] = 200
+            resources["coffee"] = 100
+            print("Ingredients have been refilled")
+        print("-----------------------------------")
+
     else:
-        drink = MENU[choice]
-        if check_resources(drink["ingredients"]):
-            payment = process_coins()
-            if check_transaction(payment, drink["cost"]):
-                make_coffee(choice, drink["ingredients"])
+        if choice == "1":
+            drink = MENU["espresso"]
+            choice = "Espresso"
+        elif choice == "2":
+            drink = MENU["latte"]
+            choice = "Latte"
+        elif choice == "3":
+            drink = MENU["cappuccino"]
+            choice = "Cappuccino"
+
+            if check_resources(drink["ingredients"]):
+                payment = process_coins()
+                if check_transaction(payment, drink["cost"]):
+                    make_coffee(choice, drink["ingredients"])
--- Day 15/Coffee Machine Project/data.py
+++ Day 15/Coffee Machine Project/data.py
@@ -24,12 +24,6 @@ MENU = {
     }
 }
 
-# resources = {
-#     "water": 300,
-#     "milk": 200,
-#     "coffee": 100,
-# }
-
 resources = {
     "water": 300,
     "milk": 200,
