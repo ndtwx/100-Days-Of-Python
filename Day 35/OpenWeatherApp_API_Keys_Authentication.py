c9813214b5b311cba28d7ab8af383f2b014c94e9
ndtwx <nd_twx@hotmail.com>
2023-12-15 20:19:04 +0800

    automatically send weather notification through whatsapp

--- Day 35/OpenWeatherApp_API_Keys_Authentication.py
+++ Day 35/OpenWeatherApp_API_Keys_Authentication.py
@@ -1,44 +1,34 @@
 import requests
 from twilio.rest import Client
 
-OWM_endpoint = "api.openweathermap.org/data/2.5/forecast"
-account_sid = "AC10bcfdd036b5d3b94b02c9a3cc67f38b"
-auth_token = "e5dbc253d46862a323f922cacaeeb45d"
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = "__YOUR_OWM_API_KEY__"
+account_sid = "__YOUR_TWILIO_ACCOUNT_ID__"
+auth_token = "__YOUR_TWILIO_AUTH_TOKEN__"
 
-parameters = {
-    "lat": 1.372580,
-    "lon": 103.893646,
+weather_params = {
+    "lat": 46.947975,
+    "lon": 7.447447,
+    "appid": api_key,
     "cnt": 4,
-    "appid": "3dd3eb982d3b816fdf8733a0f9d7f457",
 }
 
-response = requests.get(url="https://api.openweathermap.org/data/2.5/forecast", params=parameters)
+response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
-index = 0
-weather_id = []
 weather_data = response.json()
+# print(weather_data["list"][0]["weather"][0]["id"])
 
 will_rain = False
-
-# Loop through the weather id of each list
-# if the weather id is less than 700, print "Bring Umbrella"
-for hour_data in weather_data['list']:
-    condition_code = (hour_data["weather"][0]["id"])
-    if condition_code < 700:
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
         will_rain = True
-
 if will_rain:
     client = Client(account_sid, auth_token)
-    message = client.messages.create(
-        body='It is going to rain! Remember to bring an umbrella',
-        from_='whatsapp:+15185120543',
-        to='whatsapp: Your verified number'
+    message = client.messages \
+        .create(
+        body="It's going to rain today. Remember to bring an ☔️",
+        from_="YOUR TWILIO VIRTUAL NUMBER",
+        to="YOUR TWILIO VERIFIED REAL NUMBER"
     )
-
     print(message.status)
-
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-
-
-
