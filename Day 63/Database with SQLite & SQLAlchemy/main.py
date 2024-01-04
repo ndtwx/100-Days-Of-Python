7de00ec97ed0947179816d08a4523e76e2858529
ndtwx <nd_twx@hotmail.com>
2024-01-04 19:53:17 +0800

    commit

--- Day 63/Database with SQLite & SQLAlchemy/main.py
+++ Day 63/Database with SQLite & SQLAlchemy/main.py
@@ -1,44 +1,86 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+# # CREATE DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///books.db"
+# Create the extension
+db = SQLAlchemy()
+# Initialise the app with the extension
+db.init_app(app)
 
-all_books = []
+
+# # CREATE TABLE
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
 def home():
-    return render_template("index.html",books=all_books)
+    ##READ ALL RECORDS
+    # Construct a query to select from the database. Returns the rows in the database
+    # select all the records and store them in a var called records
+    records = db.session.execute(db.select(Book).order_by(Book.title))
+    # use scalars to separate the elements then set that to a new var call all_books using list comprehension
+    all_books = [record for record in records.scalars()]
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add", methods=["GET","POST"])
+@app.route("/add", methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"],
-            "author": request.form["author"],
-            "rating": request.form["rating"]
-        }
-        all_books.append(new_book)
+        # CREATE RECORD
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+
+        db.session.add(new_book)
+        db.session.commit()
         # NOTE: You can use the redirect method from flask to redirect to another route
         # e.g. in this case to the home page after the form has been submitted.
         return redirect(url_for('home'))
-
     return render_template("add.html")
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    if request.method == "POST":
+        # UPDATE RECORD
+        book_id = request.form["id"]
+        book_to_update = db.get_or_404(Book, book_id)
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for('home'))
+    book_id = request.args.get('id')
+    book_selected = db.get_or_404(Book, book_id)
+    return render_template("edit_rating.html", book=book_selected)
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+
+    # DELETE A RECORD BY ID
+    book_to_delete = db.get_or_404(Book, book_id)
+    # Alternative way to select the book to delete.
+    # book_to_delete = db.session.execute(db.select(Book).where(Book.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
