0ac4818a53fdec7b0ea43cacd423df1f88e87cc4
ndtwx <nd_twx@hotmail.com>
2024-01-08 18:43:34 +0800

    commmmit

--- Day 69/blog with users/main.py
+++ Day 69/blog with users/main.py
@@ -9,7 +9,8 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 # Import your forms from the forms.py
-from forms import CreatePostForm, RegisterForm, LoginForm
+from forms import CreatePostForm
+
 
 '''
 Make sure the required packages are installed: 
@@ -24,10 +25,6 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 
-
-
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 ckeditor = CKEditor(app)
@@ -40,26 +37,6 @@ Bootstrap5(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy()
 db.init_app(app)
-# Configure Flask-Login's Login Manager
-login_manager = LoginManager()
-login_manager.init_app(app)
-
-
-# Create a user_loader callback
-@login_manager.user_loader
-def load_user(user_id):
-    return db.get_or_404(User, user_id)
-
-# Create admin-only decorator
-def admin_only(f):
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        # If id is not 1 then return abort with 403 error
-        if current_user.id != 1:
-            return abort(403)
-        # Otherwise continue with the route function
-        return f(*args, **kwargs)
-    return decorated_function
 
 
 # CONFIGURE TABLES
@@ -74,14 +51,7 @@ class BlogPost(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
-# TODO: Create a User table for all your registered users.
-# Create a User table for all your registered users
-class User(UserMixin, db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
+# TODO: Create a User table for all your registered users. 
 
 
 with app.app_context():
@@ -90,66 +60,18 @@ with app.app_context():
 
 # TODO: Use Werkzeug to hash the user's password when creating a new user.
 @app.route('/register')
-# Register new users into the User database
-@app.route('/register', methods=["GET", "POST"])
 def register():
-    form = RegisterForm()
-    if form.validate_on_submit():
-
-        # Check if user email is already present in the database.
-        result = db.session.execute(db.select(User).where(User.email == form.email.data))
-        user = result.scalar()
-        if user:
-            # User already exists
-            flash("You've already signed up with that email, log in instead!")
-            return redirect(url_for('login'))
-
-        hash_and_salted_password = generate_password_hash(
-            form.password.data,
-            method='pbkdf2:sha256',
-            salt_length=8
-        )
-        new_user = User(
-            email=form.email.data,
-            name=form.name.data,
-            password=hash_and_salted_password,
-        )
-        db.session.add(new_user)
-        db.session.commit()
-
-        # This line will authenticate the user with Flask-Login
-        login_user(new_user)
-        return redirect(url_for("get_all_posts"))
-    return render_template("register.html", form=form)
+    return render_template("register.html")
 
 
 # TODO: Retrieve a user from the database based on their email. 
-@app.route('/login', methods=["GET", "POST"])
+@app.route('/login')
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        password = form.password.data
-        result = db.session.execute(db.select(User).where(User.email == form.email.data))
-        # Note, email in db is unique so will only have one result.
-        user = result.scalar()
-        # Email doesn't exist
-        if not user:
-            flash("That email does not exist, please register or try again.")
-            return redirect(url_for('login'))
-        # Password incorrect
-        elif not check_password_hash(user.password, password):
-            flash('Password incorrect, please try again.')
-            return redirect(url_for('login'))
-        else:
-            login_user(user)
-            return redirect(url_for('get_all_posts'))
-
-    return render_template("login.html", form=form)
+    return render_template("login.html")
 
 
 @app.route('/logout')
 def logout():
-    logout_user()
     return redirect(url_for('get_all_posts'))
 
 
@@ -168,9 +90,7 @@ def show_post(post_id):
 
 
 # TODO: Use a decorator so only an admin user can create a new post
-# Use a decorator so only an admin user can create new posts
 @app.route("/new-post", methods=["GET", "POST"])
-@admin_only
 def add_new_post():
     form = CreatePostForm()
     if form.validate_on_submit():
@@ -185,7 +105,7 @@ def add_new_post():
         db.session.add(new_post)
         db.session.commit()
         return redirect(url_for("get_all_posts"))
-    return render_template("make-post.html", form=form, current_user=current_user)
+    return render_template("make-post.html", form=form)
 
 
 # TODO: Use a decorator so only an admin user can edit a post
@@ -207,13 +127,11 @@ def edit_post(post_id):
         post.body = edit_form.body.data
         db.session.commit()
         return redirect(url_for("show_post", post_id=post.id))
-    return render_template("make-post.html", form=edit_form, is_edit=True, current_user=current_user)
+    return render_template("make-post.html", form=edit_form, is_edit=True)
 
 
 # TODO: Use a decorator so only an admin user can delete a post
-# Use a decorator so only an admin user can delete a post
 @app.route("/delete/<int:post_id>")
-@admin_only
 def delete_post(post_id):
     post_to_delete = db.get_or_404(BlogPost, post_id)
     db.session.delete(post_to_delete)
